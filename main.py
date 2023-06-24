4b37ed12a64925a2dab13d20506426d53a61cde9
David NIWEWE <davejuelz@gmail.com>
2023-06-24 20:11:13 +0200

    Added generating source files

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-import PyPDF2
+from PyPDF2 import PdfReader, PdfWriter
 
 
 def generate_source_files(sample_pdf, destination_pdf, start_page, end_page):
-    source_pdf = PyPDF2.PdfFileReader(sample_pdf)
-    destination_pdf = PyPDF2.PdfFileReader(destination_pdf)
+    source_pdf = PdfReader(sample_pdf)
+    destination_pdf = PdfWriter(destination_pdf)
     destination_pages = []
 
     # Extract the desired pages from the source PDF
     for page_number in range(start_page, end_page):
-        source_page = source_pdf.getPage(page_number)
+        source_page = source_pdf.pages[page_number]
         destination_pages.append(source_page)
 
     # Add the extracted pages to the end of the destination PDF
     for page in destination_pages:
-        destination_pdf.addPage(page)
+        destination_pdf.add_page(page)
 
-    with open('output_file.pdf', 'wb') as output_file:
+    with open(destination_pdf.fileobj, 'wb') as output_file:
         destination_pdf.write(output_file)
 
 
 def copy_pages_at_start(source_file, destination_file, page_to_copy):
-    source_pdf = PyPDF2.PdfFileReader(source_file)
-    destination_pdf = PyPDF2.PdfFileReader(destination_file)
+    source_pdf = PdfReader(source_file)
+    destination_pdf = PdfReader(destination_file)
     source_page = source_pdf.getPage(page_to_copy)
 
     # Shift existing pages in the destination PDF to make room for the new page
@@ -52,3 +52,6 @@ def run_app():
     # generate source file 2 with 40 pgs
     generate_source_files('gulliverstravels00swif.pdf',
                           'source_files/source_2.pdf', 14, 54)
+
+
+run_app()
